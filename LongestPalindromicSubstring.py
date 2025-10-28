f32e16de067406d649e8a7359f1481b6be8a878d
sharmilabachannagari <sharmila.bcgr@gmail.com>
2025-10-28 22:22:45 +0530

    Add longestPalindrome method to Solution class
    Implement a solution to find the longest palindromic substring in a given string.

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s or len(s) < 1:
+            return ""
+
+        start, end = 0, 0
+
+        def expandFromCenter(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+
+        for i in range(len(s)):
+            # Odd-length palindrome
+            left1, right1 = expandFromCenter(i, i)
+            # Even-length palindrome
+            left2, right2 = expandFromCenter(i, i + 1)
+
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+
+        return s[start:end + 1]
+
+        
